35da5c4c21596e3cfb2e5e33b8915ec3de36b238
resatugurulu <resatugur.ulu@gmail.com>
2023-09-26 10:08:11 +0300

    fix mobile cursor

--- assets/scripts/cursorScript.js
+++ assets/scripts/cursorScript.js
@@ -8,14 +8,15 @@ const smoothingFactor = 0.15;
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
 
+// Initially hide the cursor
+customCursor.style.display = 'none';
+
 // Update the custom cursor position
 function updateCustomCursor() {
   const dx = mouseX - customCursor.offsetLeft;
   const dy = mouseY - customCursor.offsetTop;
 
-  customCursor.style.left = `${
-    customCursor.offsetLeft + dx * smoothingFactor
-  }px`;
+  customCursor.style.left = `${customCursor.offsetLeft + dx * smoothingFactor}px`;
   customCursor.style.top = `${customCursor.offsetTop + dy * smoothingFactor}px`;
 
   requestAnimationFrame(updateCustomCursor);
@@ -25,6 +26,14 @@ function updateCustomCursor() {
 window.addEventListener("mousemove", (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
+
+  // Show the cursor on mouse movement
+  customCursor.style.display = 'block';
+});
+
+// Hide the cursor on touchstart (for mobile devices)
+window.addEventListener("touchstart", () => {
+  customCursor.style.display = 'none';
 });
 
 // Start updating the custom cursor
